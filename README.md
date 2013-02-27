c6b04d35a4aef3a1c781661a6673cdd6212ce933
Kevin Hamer <kevin.hamer@gmail.com>
2013-02-27 01:38:31 -0500

    initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+Overshell
+=========
+
+Overshell is a shell independent set of scripts and such meant to create a better environment for
+command line development. This includes its own set of default settings designed to improve terminal
+life as well as some mechanisms to provide shortcuts, aliases, and even alter the behavior of
+commands as you see fit.
---
+++ overshell.bash
@@ -0,0 +1,29 @@
+OVERSHELL_DIR="$HOME/Workbench/overshell"
+
+## This snippet prevents overshell from doing anything when not running interactively.
+case $- in
+	*i*) ;;
+	*) return;;
+esac
+
+## Make typing 'cd' optional
+shopt -s autocd
+
+## Look for simple spelling mistakes when changing directories and fix them.
+shopt -s cdspell
+shopt -s dirspell
+
+## Allow * to match dot-files.
+shopt -s dotglob
+
+## Allow for extended bash regex
+shopt -s extglob
+
+## Allow for ** to recursively match.
+shopt -s globstar
+
+## Add the current directory to the path.
+export PATH=$PATH:.
+
+## Run overload.
+source $OVERSHELL_DIR/lib/overload
